05d5b96420b8491c8c4f8d21ae72f826a5c44518
Ian Miell <ian.miell@gmail.com>
2015-02-26 09:42:58 +0000

    now supports more types

--- alienate.sh
+++ alienate.sh
@@ -20,18 +20,41 @@ do
 	file_type=$(file -b $f)
 	if [[ "$file_type" =~ "Debian binary package" ]]
 	then
+		echo "$f appears to be a Debian package"
 		alien $ARGS $f --to-rpm
 		alien $ARGS $f --to-slp
+		alien $ARGS $f --to-pkg
+		alien $ARGS $f --to-tgz
 	elif [[ "$file_type" =~ "RPM" ]]
 	then
+		echo "$f appears to be an RPM"
 		alien $ARGS $f --to-deb
 		alien $ARGS $f --to-slp
-	elif [[ "$file_type" =~ "bzip2 compressed" ]] && [[ $(echo $f | sed 's/.*\(....\)/\1/') = ".slp" ]]
+		alien $ARGS $f --to-pkg
+		alien $ARGS $f --to-tgz
+	#elif [[ "$file_type" =~ "bzip2 compressed" ]] && [[ $(echo $f | sed 's/.*\(....\)/\1/') = ".slp" ]]
+	#then
+	#	echo "$f appears to be a Stampede package"
+	#	alien $ARGS $f --to-deb
+	#	alien $ARGS $f --to-rpm
+	#	alien $ARGS $f --to-pkg
+	#	alien $ARGS $f --to-tgz
+	elif [[ "$file_type" =~ "pkg Datastream" ]]
 	then
+		echo "$f appears to be a Solaris package"
 		alien $ARGS $f --to-deb
 		alien $ARGS $f --to-rpm
+		alien $ARGS $f --to-slp
+		alien $ARGS $f --to-tgz
+	elif [[ "$file type" =~ "gzip compressed data" ]] && [[ $(echo $f | sed 's/.*\(....\)/\1/') = ".tgz" ]]
+	then
+		echo "assuming $f is a Slackware package"
+		alien $ARGS $f --to-deb
+		alien $ARGS $f --to-rpm
+		alien $ARGS $f --to-slp
+		alien $ARGS $f --to-pkg
 	else
-		echo "$f not a recognised package"
+		echo "$f not a supported package"
 	fi
 done
 
